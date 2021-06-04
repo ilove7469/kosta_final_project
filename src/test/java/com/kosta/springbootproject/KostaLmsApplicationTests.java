9cf7c920433a7f871639429635862edfdb68ea35
1gil2 <qazxcv8640@gmail.com>
2021-06-04 16:13:49 +0900

    project 생성

---
+++ src/test/java/com/kosta/springbootproject/KostaLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosta.springbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KostaLmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

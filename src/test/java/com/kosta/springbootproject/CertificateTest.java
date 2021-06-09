c8dc86f88fa7f0d706f4e2ad96b0d675db453887
hyk339 <hyk339@gmail.com>
2021-06-09 17:29:36 +0900

    1. CRUD Test - Certificate, Subject, Course
    2. modify Course class
    - courseTarget, coursePurpose, courseContent field's length   is changed
    from 1000 to 4000
    
    3.modify Certificate class
    - certiRegDate's type is changed from Date to LocalDateTime
    - @EntityListner annotation is added

---
+++ src/test/java/com/kosta/springbootproject/CertificateTest.java
@@ -0,0 +1,52 @@
+package com.kosta.springbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.kosta.springbootproject.model.Certificate;
+import com.kosta.springbootproject.persistence.CertificateRepository;
+
+@EnableJpaAuditing
+@SpringBootTest
+public class CertificateTest {
+
+	@Autowired
+	CertificateRepository repo;
+	
+	//@Test
+	public void insertCertificate() {
+		Certificate certi = Certificate
+				.builder()
+				.certiName("테스트")
+				.certiComment("테스트")
+				.build();
+		
+		repo.save(certi);
+
+	}
+	
+	//@Test
+	public void searchAllTest() {
+		repo.findAll().forEach(certi->{
+			System.out.println(certi.toString());
+		});
+	}
+	
+	//@Test
+	public void updateTest() {
+		 repo.findById(55L).ifPresent(d ->{
+			 d.setCertiName("산업계주도 재직자");
+			 repo.save(d);
+		 });
+	}
+	
+	@Test
+	public void deleteTest() {
+		 repo.findById(177L).ifPresent(d ->{
+			 repo.delete(d);
+		 });
+	}
+	
+}

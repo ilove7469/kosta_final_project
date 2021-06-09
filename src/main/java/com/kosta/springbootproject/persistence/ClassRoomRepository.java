679be47f1bc1fa3bee685f1d32ae1af23ec2d920
ptw <mylove7469@naver.com>
2021-06-09 10:25:44 +0900

    Teacher,ClassRoom,Lecture,LectureHall repo 생성

--- src/main/java/com/kosta/springbootproject/persistence/ClassRoomRepository.java
+++ src/main/java/com/kosta/springbootproject/persistence/ClassRoomRepository.java
@@ -2,6 +2,8 @@ package com.kosta.springbootproject.persistence;
 
 import org.springframework.data.repository.CrudRepository;
 
+import com.kosta.springbootproject.model.ClassRoom;
+
 public interface ClassRoomRepository extends CrudRepository<ClassRoom, Long>{
 
 }

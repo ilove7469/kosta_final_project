93805b5ac4abda3600d560865e3d44d38a475ba8
buchonsi <buchonsi@naver.com>
2021-06-11 18:20:27 +0900

    subjectRepository 수정

--- src/main/java/com/kosta/springbootproject/persistence/SubjectRepository.java
+++ src/main/java/com/kosta/springbootproject/persistence/SubjectRepository.java
@@ -5,9 +5,10 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 import com.kosta.springbootproject.model.Subject;
+import com.kosta.springbootproject.model.Trainee;
 
 public interface SubjectRepository extends CrudRepository<Subject, Long>{
 
-	public List<Subject> findByTraineeOrderBySubPriorityAsc(Long traineeNo);
+	public List<Subject> findByTraineeOrderBySubPriorityAsc(Trainee trainee);
 	
 }

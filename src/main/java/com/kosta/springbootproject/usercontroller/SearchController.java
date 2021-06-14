059e055f636df5772b4ed663a347d4384c072e15
1gil2 <qazxcv8640@gmail.com>
2021-06-14 17:42:28 +0900

    2021/6/14 search 구현 ing QueryInit, html, Controller

--- src/main/java/com/kosta/springbootproject/usercontroller/SearchController.java
+++ src/main/java/com/kosta/springbootproject/usercontroller/SearchController.java
@@ -1,15 +1,19 @@
 package com.kosta.springbootproject.usercontroller;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.kosta.springbootproject.model.Classes;
+import com.kosta.springbootproject.model.LectureHall;
+import com.kosta.springbootproject.model.Subject;
 import com.kosta.springbootproject.userservice.SearchService;
 
-import groovyjarjarpicocli.CommandLine.Model;
 
 @Controller
 public class SearchController {
@@ -22,9 +26,25 @@ public class SearchController {
 		
 	}
 	
-	@GetMapping("/search/search")
+	@GetMapping("/search/searchResult")
 	public void searchResult(Model model, String keyword) {
 		List<Classes> result = service.selectOption(keyword);
+		
+		HashSet<Subject> subset = new HashSet<>();
+		HashSet<LectureHall> hallset = new HashSet<>();
+		for(Classes c:result) {
+			subset.add(c.getLecture().getCourse().getSubject());
+			hallset.add(c.getClassRoom().getLectureHall());
+		}
+		List<Subject> sublist = new ArrayList<>(subset);
+		List<LectureHall> hlist = new ArrayList<>(hallset);
+		sublist.sort(null);
+		hlist.sort(null);
+		
+		model.addAttribute("hlist", hlist);
+		model.addAttribute("sublist", sublist);
+		model.addAttribute("word", keyword);
+		model.addAttribute("result", result);
 	}
 
 }

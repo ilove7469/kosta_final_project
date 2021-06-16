863d073caa63b5af20ae07e54cfd6a8bbd9603aa
ptw <mylove7469@naver.com>
2021-06-16 15:06:15 +0900

    210616 테이블 타입 변경으로 인한 작업진행중 중간 커밋 layoutadmin.html과 맡은부분 수정진행중

--- src/main/java/com/kosta/springbootproject/adminservice/ClassesService.java
+++ src/main/java/com/kosta/springbootproject/adminservice/ClassesService.java
@@ -1,9 +1,14 @@
 package com.kosta.springbootproject.adminservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.kosta.springbootproject.model.Classes;
+import com.kosta.springbootproject.model.PageVO;
 import com.kosta.springbootproject.persistence.ClassesRepository;
+import com.querydsl.core.types.Predicate;
 
 @Service
 public class ClassesService {
@@ -11,4 +16,13 @@ public class ClassesService {
 	@Autowired
 	ClassesRepository classesRepo;
 	
+	public Page<Classes> selectAll(PageVO pvo) { 
+		Predicate p = classesRepo.makePredicateClasses(pvo.getType(),pvo.getKeyword()); 
+	
+		Pageable pageable = pvo.makePaging(0, "classNo");
+		
+		Page<Classes> result = classesRepo.findAll(p, pageable);
+		return result;
+		 
+	}
 }
--- src/main/java/com/kosta/springbootproject/persistence/ClassesRepository.java
+++ src/main/java/com/kosta/springbootproject/persistence/ClassesRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.repository.CrudRepository;
 import com.kosta.springbootproject.model.Classes;
 import com.kosta.springbootproject.model.Lecture;
 import com.kosta.springbootproject.model.QClasses;
+import com.kosta.springbootproject.model.QTeacher;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Predicate;
 
@@ -32,4 +33,32 @@ public interface ClassesRepository
 		return builder;
 		
 	}
+	
+	public default Predicate makePredicateClasses(String type, String keyword) {
+		
+		BooleanBuilder builder = new BooleanBuilder();
+		QTeacher company = QTeacher.teacher;
+		builder.and(company.teacherNo.gt(0));
+		if(type==null) return builder;
+		switch (type) {
+		case "teacherId":
+			builder.and(company.teacherId.like("%" + keyword + "%"));
+			break;	
+		case "teacherName":
+			builder.and(company.teacherName.like("%" + keyword + "%"));
+			break;
+		case "teacherPhone":
+			builder.and(company.teacherPhone.like("%" + keyword + "%")); 
+			break;
+		case "teacherEmail":
+			builder.and(company.teacherEmail.like("%" + keyword + "%")); 
+			break;
+
+		default:
+			break;
+		}
+		return builder;
+		
+	}
+
 }

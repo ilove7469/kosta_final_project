4f9e768cbd20bf62a73437d1ce444e7cee5fb1eb
hyk339 <hyk339@gmail.com>
2021-06-21 18:39:17 +0900

    1. 수강신청관리 상세페이지 - 확정,취소,대기 처리 카운트 처리 완료.

--- src/main/java/com/kosta/springbootproject/adminservice/ClassHistroyService.java
+++ src/main/java/com/kosta/springbootproject/adminservice/ClassHistroyService.java
@@ -7,35 +7,83 @@ import org.springframework.stereotype.Service;
 
 import com.kosta.springbootproject.model.ClassHistory;
 import com.kosta.springbootproject.model.ClassHistoryEnumType;
+import com.kosta.springbootproject.model.Classes;
 import com.kosta.springbootproject.persistence.ClassHistoryRepository;
+import com.kosta.springbootproject.persistence.ClassesRepository;
 
 @Service
 public class ClassHistroyService {
 	
 	@Autowired
-	ClassHistoryRepository repo;
+	ClassHistoryRepository classHistoryRepo;
+	
+	@Autowired
+	ClassesRepository classesRepo;
 	
 	public List<ClassHistory> selectAll(){
-		return (List<ClassHistory>) repo.findAll();
+		return (List<ClassHistory>) classHistoryRepo.findAll();
 	}
 
-// 신청상태를 Commit으로 변경
-	public void commitManageClassDetail(Long classHistoryNo) {
-		ClassHistory classHistory = repo.findById(classHistoryNo).get();
+	public ClassHistory findClassHistoryByNo(Long classHistoryNo) {
+		ClassHistory classHistory = classHistoryRepo.findById(classHistoryNo).get();
+		return classHistory;
+	}
+	
+//  신청상태를 Commit으로 변경
+	public Long commitManageClassDetail(Long classHistoryNo) {
+		ClassHistory classHistory = classHistoryRepo.findById(classHistoryNo).get();
+		Classes classes = classesRepo.findById(classHistory.getClasses().getClassNo()).get();
+		if(classHistory.getClassHistoryState().toString()=="CANCEL") {
+			classes.setCancelCount(classes.getCancelCount()-1);
+			classes.setCommitCount(classes.getCommitCount()+1);
+		} 
+		if(classHistory.getClassHistoryState().toString()=="WAIT") {
+			classes.setWaitCount(classes.getWaitCount()-1);
+			classes.setCommitCount(classes.getCommitCount()+1);
+		} 
 		classHistory.setClassHistoryState(ClassHistoryEnumType.COMMIT);
-		repo.save(classHistory);
+		classHistoryRepo.save(classHistory);
+		classesRepo.save(classes);
+		return classHistory.getClasses().getClassNo();
 	}
-// 신청상태를 Cancel으로 변경
-	public void cancelManageClassDetail(Long classHistoryNo) {
-		ClassHistory classHistory = repo.findById(classHistoryNo).get();
+	
+//  신청상태를 Cancel으로 변경
+	public Long cancelManageClassDetail(Long classHistoryNo) {
+		ClassHistory classHistory = classHistoryRepo.findById(classHistoryNo).get();
+		Classes classes = classesRepo.findById(classHistory.getClasses().getClassNo()).get();
+		if(classHistory.getClassHistoryState().toString()=="COMMIT") {
+			classes.setCommitCount(classes.getCommitCount()-1);
+			classes.setCancelCount(classes.getCancelCount()+1);
+		} 
+		if(classHistory.getClassHistoryState().toString()=="WAIT") {
+			classes.setWaitCount(classes.getWaitCount()-1);
+			classes.setCancelCount(classes.getCancelCount()+1);
+		} 
 		classHistory.setClassHistoryState(ClassHistoryEnumType.CANCEL);
-		repo.save(classHistory);
+		classHistoryRepo.save(classHistory);
+		classesRepo.save(classes);
+		return classHistory.getClasses().getClassNo();
 	}
-// 신청상태를 Wait으로 변경
-	public void waitManageClassDetail(Long classHistoryNo) {
-		ClassHistory classHistory = repo.findById(classHistoryNo).get();
+	
+//  신청상태를 Wait으로 변경
+	public Long waitManageClassDetail(Long classHistoryNo) {
+		ClassHistory classHistory = classHistoryRepo.findById(classHistoryNo).get();
+		Classes classes = classesRepo.findById(classHistory.getClasses().getClassNo()).get();
+		if(classHistory.getClassHistoryState().toString()=="CANCEL") {
+			classes.setCancelCount(classes.getCancelCount()-1);
+			classes.setWaitCount(classes.getWaitCount()+1);
+		} 
+		if(classHistory.getClassHistoryState().toString()=="COMMIT") {
+			classes.setCommitCount(classes.getCommitCount()-1);
+			classes.setWaitCount(classes.getWaitCount()+1);
+		} 
 		classHistory.setClassHistoryState(ClassHistoryEnumType.WAIT);
-		repo.save(classHistory);
+		classHistoryRepo.save(classHistory);
+		classesRepo.save(classes);
+		return classHistory.getClasses().getClassNo();
 	}
+	
+
+	
 
 }
